83f54f45fc97bf39b75b1ebd68d77e84204edcbb
AlanProphettSoftwire <alan.prophett@softwire.com>
2025-09-09 17:19:46 +0100

    add Bang and Bong case

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,24 @@
+function mod(n, m) {
+    return n % m == 0;
+}
+
+function is_bong_case(n) {
+    if (mod(n, 11)) {
+        return true;
+    }
+    return false;
+}
+
 function fizzbuzz() {
-    conditions = [num => num % 3 === 0, num => num % 5 === 0];
-    partial_strings = ["Fizz", "Buzz"];
+    conditions = [num => mod(num,3), num => mod(num,5), num => mod(num,7)];
+    partial_strings = ["Fizz", "Buzz", "Bang"];
 
     for (let i = 1; i <= 100; i++) {
+        if (is_bong_case(i)) {
+            console.log("Bong");
+            continue;
+        }
+
         let output = "";
         for (let j = 0; j < conditions.length; j++) {
             if (conditions[j](i)) {
